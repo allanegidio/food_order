c128a1f470a1011929da669de981d0d56f642871
Allan Egidio <allan.egidio@outlook.com>
2023-06-20 20:55:59 -0300

    refactor: use products from database on list

--- lib/food_order_web/live/page_live/page_live.ex
+++ lib/food_order_web/live/page_live/page_live.ex
@@ -1,3 +1,14 @@
 defmodule FoodOrderWeb.PageLive do
   use FoodOrderWeb, :live_view
+
+  alias FoodOrder.Products
+  alias FoodOrderWeb.PageLive.ProductItem
+
+  def mount(_params, _session, socket) do
+    products = Products.list_products()
+
+    socket = assign(socket, products: products)
+
+    {:ok, socket}
+  end
 end
