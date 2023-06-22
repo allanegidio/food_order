af478680ea15a637a0c249673d564a072c153cbc
Allan Egidio <allan.egidio@outlook.com>
2023-06-22 20:04:46 -0300

    feat: add cart live

---
+++ lib/food_order_web/live/cart_live/cart_live.ex
@@ -0,0 +1,28 @@
+defmodule FoodOrderWeb.CartLive do
+  use FoodOrderWeb, :live_view
+
+  alias FoodOrderWeb.CartLive.CartDetails
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, total: 0)}
+  end
+
+  def empty_cart(assigns) do
+    ~H"""
+    <div class="py-16 mx-auto text-center">
+      <h1 class="text-3xl font-bold mb-2">Your cart is empty!</h1>
+      <p class="text-neutral-500 text-lg mb-12">
+        You probably haven't ordered a food yet.
+        To order an goood food, go to the main page.
+      </p>
+      <Heroicons.shopping_bag solid class="w-20 h-20 mx-auto text-red-500" />
+      <a
+        href={~p"/"}
+        class="inline-block px-6 py-2 mt-12 font-bold rounded-full bg-red-500 text-white "
+      >
+        Go back
+      </a>
+    </div>
+    """
+  end
+end
