813c653967888b6193f936d817a428814e89ee02
Allan Egidio <allan.egidio@outlook.com>
2023-06-27 18:14:14 -0300

    feat(account): add plug to validate admin role

---
+++ lib/food_order_web/plugs/require_admin.ex
@@ -0,0 +1,18 @@
+defmodule FoodOrderWeb.RequireAdmin do
+  use FoodOrderWeb, :verified_routes
+
+  import Phoenix.Component
+
+  def on_mount(:default, _params, _session, socket) do
+    current_user = socket.assigns.current_user
+
+    if current_user.role == :ADMIN do
+      {:cont, socket}
+    else
+      {:halt,
+       socket
+       |> Phoenix.LiveView.put_flash(:error, "You must be admin to access this page!")
+       |> Phoenix.LiveView.redirect(to: ~p"/")}
+    end
+  end
+end
