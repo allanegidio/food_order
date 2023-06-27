1fbdcfd188246184cfeb12c4d70597d335fe28c8
Allan Egidio <allan.egidio@outlook.com>
2023-06-27 16:32:19 -0300

    feat(account): add user on seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,15 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias FoodOrder.Accounts
 alias FoodOrder.Products
 alias FoodOrder.Products.Product
 
-for _ <- 1..10 do
-  Products.create_product(%{
-    description: "Some Description #{System.unique_integer([:positive])}",
-    name: "Some Product #{System.unique_integer([:positive])}",
-    price: System.unique_integer([:positive]),
-    size: Enum.random(Product.size_values()),
-    image_url: "product_#{Enum.random(1..5)}.jpeg"
-  })
-end
+for _ <- 1..10,
+    do:
+      Products.create_product(%{
+        description: "Some Description #{System.unique_integer([:positive])}",
+        name: "Some Product #{System.unique_integer([:positive])}",
+        price: System.unique_integer([:positive]),
+        size: Enum.random(Product.size_values()),
+        image_url: "product_#{Enum.random(1..5)}.jpeg"
+      })
+
+Accounts.register_user(%{
+  email: "admin@domain.com",
+  password: "admin@domain.com",
+  role: "ADMIN"
+})
+
+Accounts.register_user(%{
+  email: "user@domain.com",
+  password: "user@domain.com",
+  role: "USER"
+})
