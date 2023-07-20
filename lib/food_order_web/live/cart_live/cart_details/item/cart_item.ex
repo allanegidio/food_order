9b4bfbfe85e53512d6ee5d4c1f46c3d46182ab41
Allan Egidio <allan.egidio@outlook.com>
2023-07-20 14:45:48 -0300

    refactor: add test cart feature

--- lib/food_order_web/live/cart_live/cart_details/item/cart_item.ex
+++ lib/food_order_web/live/cart_live/cart_details/item/cart_item.ex
@@ -38,25 +38,33 @@ defmodule FoodOrderWeb.CartLive.CartDetails.Item.CartItem do
 
   def render(assigns) do
     ~H"""
-    <div class="flex items-center my-8 shadow-lg p-2 hover:bg-neutral-200">
-      <img src={~p"/images/#{@item.item.image_url}"} class="h-16 w-16 rounded-full" />
+    <div id={@id} data-role="item" class="flex items-center my-8 shadow-lg p-2 hover:bg-neutral-200">
+      <img
+        data-role="item-image"
+        src={~p"/images/#{@item.item.image_url}"}
+        class="h-16 w-16 rounded-full"
+      />
 
       <div class="flex-1 ml-4">
         <h1><%= @item.item.name %></h1>
         <span><%= @item.item.size %></span>
       </div>
 
-      <div class="flex-1">
+      <div class="flex-1" data-role="quantity">
         <div class="flex items-center">
           <button
+            data-role="dec"
+            data-id={@id}
             phx-click="dec"
             phx-target={@myself}
             class="p-1 m-2 rounded-full font-bold text-white bg-red-500"
           >
             -
           </button>
-          <span><%= @item.qty %> Items(s)</span>
+          <span data-role="items" data-id={@id}><%= @item.qty %> Item(s)</span>
           <button
+            data-role="inc"
+            data-id={@id}
             phx-click="inc"
             phx-target={@myself}
             class="p-1 m-2 rounded-full font-bold text-white bg-red-500"
@@ -66,9 +74,15 @@ defmodule FoodOrderWeb.CartLive.CartDetails.Item.CartItem do
         </div>
       </div>
 
-      <div class="flex flex-col">
-        <span class="font-bold text-lg"><%= @item.item.price %></span>
-        <button class="mx-auto" phx-click="remove" phx-target={@myself}>
+      <div class="flex flex-col" data-role="total-item">
+        <span class="font-bold text-lg" data-role="price" data-id={@id}><%= @item.item.price %></span>
+        <button
+          class="mx-auto"
+          phx-click="remove"
+          data-role="remove"
+          data-id={@id}
+          phx-target={@myself}
+        >
           <Heroicons.trash solid class="w-8 h-8 text-red-500" />
         </button>
       </div>
