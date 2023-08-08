dbc63990cc28d2d517b3e77a8e37d6b7af737b77
Allan Egidio <allan.egidio@outlook.com>
2023-08-08 14:23:33 -0300

    feat: test index order

---
+++ test/food_order_web/live/customer/order_live/index_test.exs
@@ -0,0 +1,20 @@
+defmodule FoodOrderWeb.Customer.OrderLive.IndexTest do
+  use FoodOrderWeb.ConnCase
+
+  alias FoodOrder.ProductsFixtures
+  alias FoodOrder.OrdersFixtures
+
+  import Phoenix.LiveViewTest
+
+  describe "load page" do
+    setup :register_and_log_in_user
+
+    test "check order list", %{conn: conn, user: user} do
+      order = OrdersFixtures.order_with_items_fixture(user)
+
+      {:ok, view, _html} = live(conn, ~p"/customer/orders")
+
+      assert has_element?(view, "##{order.id}")
+    end
+  end
+end
