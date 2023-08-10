24a3db721df39f026c2bd88b0cb212f102ebbcbf
Allan Egidio <allan.egidio@outlook.com>
2023-08-10 14:39:02 -0300

    refactor(customer): add more test on index order live

--- test/food_order_web/live/customer/order_live/index_test.exs
+++ test/food_order_web/live/customer/order_live/index_test.exs
@@ -15,5 +15,18 @@ defmodule FoodOrderWeb.Customer.OrderLive.IndexTest do
 
       assert has_element?(view, "##{order.id}")
     end
+
+    test "send to another layer using the handle_info using pid", %{conn: conn, user: user} do
+      order = OrdersFixtures.order_with_items_fixture(user)
+
+      {:ok, view, _html} = live(conn, ~p"/customer/orders")
+
+      assert view |> element("td", "Not started") |> render() =~ "text-red-500"
+
+      order = Map.put(order, :status, :delivered)
+      send(view.pid, {:updated_user_order, order})
+
+      refute view |> element("td", "Delivered") |> render() =~ "text-red-500"
+    end
   end
 end
