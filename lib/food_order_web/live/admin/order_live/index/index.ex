cee5cf43dde287b777fe3a4d1aa11107c8e100d8
Allan Egidio <allan.egidio@outlook.com>
2023-08-11 15:29:15 -0300

    refactor: fix some tests and remove unused code

--- lib/food_order_web/live/admin/order_live/index/index.ex
+++ lib/food_order_web/live/admin/order_live/index/index.ex
@@ -2,8 +2,6 @@ defmodule FoodOrderWeb.Admin.OrderLive.Index do
   use FoodOrderWeb, :live_view
 
   alias FoodOrder.Orders
-  alias FoodOrder.Orders.Order
-
   alias FoodOrderWeb.Admin.OrderLive.Index.SideMenu
   alias FoodOrderWeb.Admin.OrderLive.Index.Layer
 
@@ -17,6 +15,7 @@ defmodule FoodOrderWeb.Admin.OrderLive.Index do
     {:ok, stream(socket, :orders, Orders.list_orders())}
   end
 
+  @impl true
   def handle_info({:updated_admin_order, %{status: new_status}, old_status}, socket) do
     send_update(Layer, id: old_status)
     send_update(Layer, id: Atom.to_string(new_status))
@@ -25,46 +24,11 @@ defmodule FoodOrderWeb.Admin.OrderLive.Index do
     {:noreply, socket}
   end
 
+  @impl true
   def handle_info({:new_order, %{status: status}}, socket) do
     send_update(Layer, id: Atom.to_string(status))
     send_update(SideMenu, id: "side-menu")
 
     {:noreply, socket}
-  end
-
-  @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Order")
-    |> assign(:order, Orders.get_order!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Order")
-    |> assign(:order, %Order{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Orders")
-    |> assign(:order, nil)
-  end
-
-  @impl true
-  def handle_info({FoodOrderWeb.OrderLive.FormComponent, {:saved, order}}, socket) do
-    {:noreply, stream_insert(socket, :orders, order)}
-  end
-
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    order = Orders.get_order!(id)
-    {:ok, _} = Orders.delete_order(order)
-
-    {:noreply, stream_delete(socket, :orders, order)}
   end
 end
--- test/food_order_web/live/admin/order_live/order_live_test.exs
+++ test/food_order_web/live/admin/order_live/order_live_test.exs
@@ -4,20 +4,6 @@ defmodule FoodOrderWeb.Admin.OrderLiveTest do
   import Phoenix.LiveViewTest
   import FoodOrder.OrdersFixtures
 
-  @create_attrs %{
-    address: "some address",
-    phone_number: "some phone_number",
-    total_price: 42,
-    total_quantity: 42
-  }
-  @update_attrs %{
-    address: "some updated address",
-    phone_number: "some updated phone_number",
-    total_price: 43,
-    total_quantity: 43
-  }
-  @invalid_attrs %{address: nil, phone_number: nil, total_price: nil, total_quantity: nil}
-
   defp create_order(_) do
     order = order_fixture()
     %{order: order}
@@ -39,54 +25,6 @@ defmodule FoodOrderWeb.Admin.OrderLiveTest do
       assert has_element?(index_live, "#preparing")
       assert has_element?(index_live, "#delivering")
       assert has_element?(index_live, "#delivered")
-    end
-
-    @tag :skip
-    test "saves new order", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/admin/orders")
-
-      assert index_live |> element("a", "New Order") |> render_click() =~
-               "New Order"
-
-      assert_patch(index_live, ~p"/orders/new")
-
-      assert index_live
-             |> form("#order-form", order: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#order-form", order: @create_attrs)
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/admin/orders")
-
-      html = render(index_live)
-      assert html =~ "Order created successfully"
-      assert html =~ "some address"
-    end
-
-    @tag :skip
-    test "updates order in listing", %{conn: conn, order: order} do
-      {:ok, index_live, _html} = live(conn, ~p"/admin/orders")
-
-      assert index_live |> element("#orders-#{order.id} a", "Edit") |> render_click() =~
-               "Edit Order"
-
-      assert_patch(index_live, ~p"/orders/#{order}/edit")
-
-      assert index_live
-             |> form("#order-form", order: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#order-form", order: @update_attrs)
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/admin/orders")
-
-      html = render(index_live)
-      assert html =~ "Order updated successfully"
-      assert html =~ "some updated address"
     end
   end
 end
